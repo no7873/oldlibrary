91eee2fe4344f6b1df8c98181a2aaa2e0f13596c
jaey3325 <tjy0711@naver.com>
2021-11-11 17:17:19 +0900

    rcart 수정1

--- rcart/views.py
+++ rcart/views.py
@@ -8,21 +8,21 @@ from .cart import Rcart
 
 @require_POST
 def add(request, product_id):
-    cart = Rcart(request)
+    rcart = Rcart(request)
     product = get_object_or_404(Rentbook, id=product_id)
 
     form = AddProductForm(request.POST)
 
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add(product=product, quantity=cd['quantity'], is_update=cd['is_update'])
+        rcart.add(product=product, quantity=cd['quantity'], is_update=cd['is_update'])
 
     return redirect('rcart:detail')
 
 def remove(request, product_id):
-    cart = Rcart(request)
+    rcart = Rcart(request)
     product = get_object_or_404(Rentbook, id=product_id)
-    cart.remove(product)
+    rcart.remove(product)
 
     return redirect('rcart:detail')
 
